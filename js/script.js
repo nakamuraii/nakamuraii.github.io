a2e5c29b5baa572a0391786797fb8ca789732a69
nakamura <134006771+nakamuraii@users.noreply.github.com>
2024-02-16 22:40:16 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+const menuBtn = document.getElementById('menuButton');
+const closeBtn = document.querySelector('.closeButton');
+const mobileMenu = document.getElementById('mobileMenu');
+const mobileLinks = document.querySelectorAll('.closeButton.menu__link');
+
+function showMenu() {
+    mobileMenu.classList.remove('closed-menu')
+    mobileMenu.classList.add('open-menu')
+}
+
+function closeMenu() {
+    mobileMenu.classList.remove('open-menu')
+    mobileMenu.classList.add('closed-menu')
+}
+
 const textToType = "Front-End React Developer";
 const typedTextElement = document.getElementById("typed-text");
 
@@ -18,3 +33,9 @@ function typeText() {
 }
  
 typeText();
+menuBtn.addEventListener('click', showMenu)
+closeBtn.addEventListener('click', closeMenu)
+mobileLinks.forEach(link => link.addEventListener('click', closeMenu));
+
+
+
